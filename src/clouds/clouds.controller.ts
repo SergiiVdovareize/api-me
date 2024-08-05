7de04eb519110b68b2ae7fa5b8c6b731b1e38c44
Sergii Vdovareize <sergii.vdovareize@inspera.no>
2024-08-05 17:15:20 +0300

    read results

--- src/clouds/clouds.controller.ts
+++ src/clouds/clouds.controller.ts
@@ -26,4 +26,49 @@ export class CloudsController {
     await this.requestService.registerArmstrongApiCall();
     return this.cloudsService.getArmstrongNumber(index);
   }
+
+  @Get('/result/:id')
+  async result(@Param('id') id: string) {
+    const maxWaitTime = 60*60*1000; // 60 minutes
+    const invalidIdResponse = {
+        success: false,
+        status: 1,
+        message: 'result id is not valid'
+    }
+    const reg = /\w{2}(\d{1})\w{2}(\d{3})\w{2}(\d{3})\w{2}(\d{3})\w{2}(\d{3})\w{2}/;
+    const groups = id.match(reg);
+
+    if (!groups || groups.length !== 6) {
+      return invalidIdResponse
+    }
+
+    const timestamp = parseInt(`${groups[3]}${groups[4]}${groups[5]}${groups[2]}${groups[1]}`, 10)
+    if (isNaN(timestamp)) {
+      return invalidIdResponse
+    }
+
+    const maxTimestamp = Date.now();
+    if (timestamp < (maxTimestamp - maxWaitTime) || timestamp > maxTimestamp) {
+      return invalidIdResponse
+    }
+
+    const resultFileUrl = await this.cloudsService.findResultFileUrlWithRetry(id)
+    if (!resultFileUrl) {
+      return {
+        success: false,
+        status: 2
+      }
+    }
+
+    const result = await this.cloudsService.readResult(resultFileUrl)
+    if (!result) {
+      return {
+        success: false,
+        status: 3,
+        message: 'could bot read result'
+      }
+    }
+
+    return result;
+  }
 }
--- src/clouds/clouds.service.ts
+++ src/clouds/clouds.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@nestjs/common';
-import { put, del, list } from '@vercel/blob';
+import { put, del, list, ListFoldedBlobResult } from '@vercel/blob';
 import { uid } from 'uid';
 import { env } from 'process';
 
@@ -52,13 +52,17 @@ export class CloudsService {
 
     generateFilename() {
         const base = Date.now().toString().match(/.{1,3}/g).reverse()
-        const fileName = `${uid(2)}${base[0]}${uid(2)}${base[1]}${uid(2)}${base[2]}${uid(2)}${base[3]}${uid(2)}${base[4]}${uid(2)}`
+        const fileName = `${uid(2)}${base[0]}${uid(2)}${base[1]}${uid(2)}${base[4]}${uid(2)}${base[3]}${uid(2)}${base[2]}${uid(2)}`
         return fileName;
     }
 
     async getNumber(url: string) {
         const data = await fetch(url)
         return await data.json();
+    }
+
+    removeResultFile(url: string): void {
+        del(url);
     }
 
     async createResultFile(filename: string, data: JSON): Promise<string> {
@@ -68,5 +72,41 @@ export class CloudsService {
         });
     
         return blob.url;
+    }
+
+    async findResultFileUrl(id: string): Promise<string|null> {
+        const fileList: ListFoldedBlobResult = await list({prefix: id});
+        return fileList.blobs.length === 1 ? fileList.blobs[0].url : null
+    }
+
+    async findResultFileUrlWithRetry(id: string, attempt: number = 0): Promise<string|null> {
+        const maxTries = 7;
+        const retryDelay = 500;
+        return new Promise(async resolve => {
+            const url = await this.findResultFileUrl(id)
+            // console.log('attempt - ', attempt, id, url)
+            if (url) {
+                resolve(url)
+                return;
+            }
+            if (!url && attempt < maxTries) {
+                setTimeout(async () => {
+                    resolve(await this.findResultFileUrlWithRetry(id, attempt + 1))
+                }, retryDelay)
+            } else {
+                resolve(null)
+            }
+        })
+    }
+
+    async readResult(url: string): Promise<JSON> {
+        try {
+            const data = await fetch(url)
+            const json = await data.json()
+            this.removeResultFile(url)
+            return json;
+        } catch (error) {
+            return null
+        }
     }
 }
