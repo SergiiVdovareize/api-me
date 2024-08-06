556d21cb3d34328390565ac0a89d8c8e5e02851a
Sergii Vdovareize <sergii.vdovareize@inspera.no>
2024-08-06 12:02:40 +0300

    cleanup

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,17 +1,14 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
-import { RequestsService } from './requests/requests.service';
 
 @Controller()
 export class AppController {
   constructor(
     private readonly appService: AppService,
-    private readonly requestService: RequestsService,
   ) {}
 
   @Get()
   async getHello(): Promise<string> {
-    await this.requestService.registerPlainApiCall();
     return this.appService.getHello();
   }
 }
