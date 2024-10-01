3177052f4c6905290487ee442a15fb8044126783
Sergii Vdovareize <sergii.vdovareize@inspera.no>
2024-10-01 08:42:51 +0300

    sentry

--- src/instrument.ts
+++ src/instrument.ts
@@ -1,11 +1,9 @@
-import * as Sentry from "@sentry/nestjs"
-import { nodeProfilingIntegration } from "@sentry/profiling-node";
+import * as Sentry from '@sentry/nestjs';
+import { nodeProfilingIntegration } from '@sentry/profiling-node';
 
 Sentry.init({
-  dsn: "https://712327fef6cf78fb05847c2b58e81fef@o4508043966087168.ingest.de.sentry.io/4508043969298512",
-  integrations: [
-    nodeProfilingIntegration(),
-  ],
+  dsn: 'https://712327fef6cf78fb05847c2b58e81fef@o4508043966087168.ingest.de.sentry.io/4508043969298512',
+  integrations: [nodeProfilingIntegration()],
   // Tracing
   tracesSampleRate: 1.0, //  Capture 100% of the transactions
 
