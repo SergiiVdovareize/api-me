f1349af0d017f7066c947fa1ae7beded1393e715
Sergii Vdovareize <sergii.vdovareize@inspera.no>
2025-03-07 18:51:12 +0200

    tracker

--- src/track/track.controller.ts
+++ src/track/track.controller.ts
@@ -5,6 +5,27 @@ import { TrackService } from './track.service';
 export class TrackController {
   constructor(private readonly trackService: TrackService) {}
 
+  @Get('')
+  async track() {
+    console.log('track 0')
+    setTimeout(() => {
+      console.log('track 10')
+    }, 10000);
+    setTimeout(() => {
+      console.log('track 20')
+    }, 20000);
+    setTimeout(() => {
+      console.log('track 30')
+    }, 30000);
+    setTimeout(() => {
+      console.log('track 40')
+    }, 40000);
+    setTimeout(() => {
+      console.log('track 50')
+    }, 50000);
+    return {success: true}
+  }
+
   @Get('mono/:id')
   async findOne(@Param('id') id: string, @Query('plain') plain: string) {
     return await this.trackService.findOne(id, ["true", "1"].includes(plain));
