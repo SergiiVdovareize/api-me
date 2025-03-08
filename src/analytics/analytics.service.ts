bd8b912dc50f98345a54a6037351f6f157228677
Sergii Vdovareize <sergii.vdovareize@inspera.no>
2025-03-08 09:00:27 +0200

    env

--- src/analytics/analytics.service.ts
+++ src/analytics/analytics.service.ts
@@ -1,11 +1,13 @@
 import { Injectable } from '@nestjs/common';
+import { env } from 'process';
 import { PosthogService } from 'src/posthog/posthog.service';
 
 @Injectable()
 export class AnalyticsService {
   constructor(private posthogService: PosthogService) {}
 
-  trackEvent(event: string, properties: Record<string, any> = {}) {
+  trackEvent(event: string, properties: Record<string, any> = {}, forceEnv: boolean = false) {
+    console.log('env', env.HOST);
     this.posthogService.trackEvent(event, properties);
   }
 }
