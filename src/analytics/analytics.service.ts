1e3594b801bcf5e298cb4040616d49f459bc40c7
Sergii Vdovareize <sergii.vdovareize@inspera.no>
2025-03-08 09:10:40 +0200

    log

--- src/analytics/analytics.service.ts
+++ src/analytics/analytics.service.ts
@@ -7,10 +7,12 @@ export class AnalyticsService {
   constructor(private posthogService: PosthogService) {}
 
   trackEvent(event: string, properties: Record<string, any> = {}, forceEnv: boolean = false) {
-    if(env.HOST !== 'prod' && !forceEnv) {
+    if (env.HOST !== 'prod' && !forceEnv) {
+      console.log('event filtered', env.HOST)
       return;
     }
-    
+
+    console.log('event tracked', event)
     this.posthogService.trackEvent(event, properties);
   }
 }
