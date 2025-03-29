4759658f7fe1907155e6df253e8cf00bc54a87f1
Sergii Vdovareize <servdo.gmail.com>
2025-03-29 23:28:27 +0200

    scheme

---
+++ prisma/migrations/20250329212631_baseline_2/migration.sql
@@ -0,0 +1,28 @@
+-- CreateTable
+CREATE TABLE "Account" (
+    "id" SERIAL NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "isActive" BOOLEAN NOT NULL DEFAULT true,
+    "trackId" TEXT NOT NULL,
+    "type" "AccountType" NOT NULL,
+    "title" TEXT,
+    "description" TEXT,
+
+    CONSTRAINT "Account_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "AccountIncoming" (
+    "id" SERIAL NOT NULL,
+    "balance" INTEGER NOT NULL,
+    "trackedAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "accountId" INTEGER NOT NULL,
+
+    CONSTRAINT "AccountIncoming_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Account_trackId_key" ON "Account"("trackId");
+
+-- AddForeignKey
+ALTER TABLE "AccountIncoming" ADD CONSTRAINT "AccountIncoming_accountId_fkey" FOREIGN KEY ("accountId") REFERENCES "Account"("id") ON DELETE CASCADE ON UPDATE CASCADE;
