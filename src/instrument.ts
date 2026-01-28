48c85b745e5e3a7beb4dd5dd683b78822f02f806
SergiiVdovareize <servdo@gmail.com>
2026-01-28 17:05:20 +0200

    disabled sentry

--- src/instrument.ts
+++ src/instrument.ts
@@ -1,12 +1,1 @@
-import * as Sentry from '@sentry/nestjs';
-import { nodeProfilingIntegration } from '@sentry/profiling-node';
-
-Sentry.init({
-  dsn: 'https://712327fef6cf78fb05847c2b58e81fef@o4508043966087168.ingest.de.sentry.io/4508043969298512',
-  integrations: [nodeProfilingIntegration()],
-  // Tracing
-  tracesSampleRate: 1.0, //  Capture 100% of the transactions
-
-  // Set sampling rate for profiling - this is relative to tracesSampleRate
-  profilesSampleRate: 1.0,
-});
+// Sentry error tracking is disabled
