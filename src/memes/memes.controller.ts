fcf3e7a7e208bdd6ce25b458c38025344ea1f53c
SergiiVdovareize <servdo@gmail.com>
2026-06-02 11:26:34 +0300

    refactor: remove unused RequestsService dependency and cleanup dead code in MemesController

--- src/memes/memes.controller.ts
+++ src/memes/memes.controller.ts
@@ -1,20 +1,12 @@
 import { Controller, Get, Param } from '@nestjs/common';
-import { RequestsService } from 'src/requests/requests.service';
 import { MemesService } from './memes.service';
 
 @Controller('memes')
 export class MemesController {
-  constructor(
-    private readonly memesService: MemesService,
-    private readonly requestsService: RequestsService
-  ) {}
+  constructor(private readonly memesService: MemesService) {}
 
   @Get(':url')
   async steal(@Param('url') url: string): Promise<object> {
-    // const result = await this.memesService.steelFromPubler(url);
-    // const result = await this.memesService.steelFromSquidlr(url);
-    // const result = await this.memesService.steelFromGetInDevice(url)
-    // const result = await this.memesService.steelFromSnap(url)
     const result = await this.memesService.stealMeme(url);
 
     return result;
