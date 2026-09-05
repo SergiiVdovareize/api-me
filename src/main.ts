f2ee42a7b9b887d151cd6532a8867c10b0af7480
SergiiVdovareize <servdo@gmail.com>
2026-09-06 00:03:16 +0300

    Revert "feat: increase Vercel execution timeout to 60 seconds"
    This reverts commit ab6db89284362736105269cc42ed951e5b8579b0.

--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,6 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { env } from 'process';
 
-export const config = {
-  maxDuration: 60,
-};
-
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
